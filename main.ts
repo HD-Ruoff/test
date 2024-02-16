9fd32b19120941cb5f08156904fbd77f775fc95a
HD-Ruoff <160256185+HD-Ruoff@users.noreply.github.com>
2024-02-16 18:50:45 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+	
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+	
+})
 basic.forever(function () {
     music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
 })

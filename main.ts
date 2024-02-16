435f79e8bbfa2e95d4f6a5a85be769b9908c99da
HD-Ruoff <160256185+HD-Ruoff@users.noreply.github.com>
2024-02-16 18:47:44 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,3 @@
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    basic.showString("hi!")
-})
-input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    basic.showIcon(IconNames.Heart)
-})
 basic.forever(function () {
-	
+    music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
 })

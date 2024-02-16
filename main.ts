f83061bd9aa2a1b51a69e39a534db372cac7ae4e
HD-Ruoff <160256185+HD-Ruoff@users.noreply.github.com>
2024-02-16 18:44:52 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    basic.showString("hi!")
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.showIcon(IconNames.Heart)
+})
 basic.forever(function () {
 	
 })
